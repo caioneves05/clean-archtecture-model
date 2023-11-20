e897e84a0eba6f87f48d3b2969a7cf0f258342e7
caioneves05 <ncaio037@gmail.com>
2023-11-20 20:51:36 -0300

    fix: prisma binary targests

--- src/server/containers/user-container.ts
+++ src/server/containers/user-container.ts
@@ -2,9 +2,10 @@ import 'reflect-metadata';
 import { container } from 'tsyringe';
 import { CreateUserUseCase } from '../../app/useCases/user/createUsersUseCase';
 import { UserController } from '../../infra/http/controllers/user.controller';
+import { PrismaUserRepository } from '../../infra/database/prisma/repositories/PrismaUserRepository';
 
 container.register('CreateUserUseCase', { useClass: CreateUserUseCase });
-// import ORM class here
+container.register('UserRepository', { useClass: PrismaUserRepository });
 
 const userResolver = container.resolve(UserController);
 
